3f11a6cd9c3b696cdec467848a5b5f93849b37cd
xbac2k4 <162277991+xbac2k4@users.noreply.github.com>
2024-09-30 15:26:17 +0700

    1
    1

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var createError = require('http-errors');
 var express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
+//
+const database = require('./config/db');
 
 var app = express();
 
 // view engine setup
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true, // allows access to prototype properties
+    allowProtoMethodsByDefault: true,    // allows access to prototype methods
+  },
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -21,6 +38,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+//
+
+// Cấu hình session middleware
+app.use(session({
+  secret: 'CINEMA',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } // Đặt secure: true nếu bạn sử dụng HTTPS
+}));
+// Cấu hình body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//
+// app.post('/upload-avatar', upload.single('image'), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).send('No file uploaded.');
+//   }
+//   // Xử lý tệp tải lên ở đây
+//   res.send('Tệp đã được tải lên thành công');
+// });
+//
+database.connect();
+//
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
